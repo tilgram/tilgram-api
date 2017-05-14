f013d8df3ac18f18c83aa21dc0f06ef76f75e838
Omid Hezaveh <omid.hezaveh@futurice.com>
2017-05-14 21:33:49 +0300

    Add koa/postgres

---
+++ src/bin/server.js
@@ -0,0 +1,23 @@
+const config = require('../config')
+
+const app = require('../app')
+
+process.once('SIGINT', shutDown)
+process.once('SIGTERM', shutDown)
+
+console.log(`Spinning the server on:
+http(s)://${config.server.host}:${config.server.port}
+  `)
+app.server.listen(config.server.port, config.server.host)
+
+function shutDown () {
+  console.log('Shutdown')
+  if (app.server.listening) {
+    app.server.shutdown(error => {
+      if (error) {
+        console.error(error)
+      }
+      app.db.destroy().catch(console.error)
+    })
+  }
+}
